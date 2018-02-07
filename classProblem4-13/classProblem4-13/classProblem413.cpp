3dd168b3f3cd07f9de8be8c33bea47c29caaa5c9
DockingMule <aca0824@gmail.com>
2018-02-06 19:39:40 -0500

    Add files via upload

---
+++ classProblem4-13/classProblem4-13/classProblem413.cpp
@@ -0,0 +1,43 @@
+// This program uses an if/else if statement to assign a
+// letter grade (A, B, C, D, or F) to a numeric test score.
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	// Constants for grade thresholds
+	const int A_SCORE = 90,
+		B_SCORE = 80,
+		C_SCORE = 70,
+		D_SCORE = 60;
+
+	int testScore;  // To hold a numeric test score
+	bool gradeStatus = true;
+					// Get the numeric test score.
+	cout << "Enter your numeric test score and I will\n"
+		<< "tell you the letter grade you earned: ";
+	cin >> testScore;
+	system("cls");
+	// Determine the letter grade.
+	if (testScore > 100)
+		gradeStatus = false;
+	else if (testScore >= A_SCORE)
+		cout << "Your grade is A.\n";
+	else if (testScore >= B_SCORE)
+		cout << "Your grade is B.\n";
+	else if (testScore >= C_SCORE)
+		cout << "Your grade is C.\n";
+	else if (testScore >= D_SCORE)
+		cout << "Your grade is D.\n";
+	else if (testScore >= 0)
+		cout << "Your grade is F.\n";
+	else
+		gradeStatus = false;
+	if (gradeStatus)
+		cout << "It's a valid score.\n";
+	else
+		cout << "It is not a valide score\n";
+	
+	return 0;
+}
